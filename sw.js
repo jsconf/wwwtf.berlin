dad2e612255867696c27ed0757e6787567d892af
Paul Verbeek-Mast <verbeek.p@gmail.com>
2017-06-15 09:28:03 +0200

    added wwwtf.berlin in footer

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 this.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
+    caches.open('v2').then(function (cache) {
       return cache.addAll([
         '/',
         '/index.html',
@@ -20,7 +20,7 @@ this.addEventListener('fetch', function (event) {
   event.respondWith(caches.match(event.request).catch(function () {
     return fetch(event.request);
   }).then(function (response) {
-    caches.open('v1').then(function (cache) {
+    caches.open('v2').then(function (cache) {
       cache.put(event.request, response);
     });
     return response.clone();
