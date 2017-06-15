c165452f16e47cfb2dd01368f72d3b3d8fc843aa
Paul Verbeek-Mast <verbeek.p@gmail.com>
2017-06-15 13:08:10 +0200

    fix cache updating

--- sw.js
+++ sw.js
@@ -8,8 +8,11 @@ this.addEventListener('install', function (event) {
         '/index.html',
         '/css/style.css',
         '/js/script.js',
+        '/about/',
         '/about/index.html',
+        '/add-your-event/',
         '/add-your-event/index.html',
+        '/attendee-guide/',
         '/attendee-guide/index.html',
         '/fonts/GT-Walsheim-Medium.woff',
         '/fonts/GT-Walsheim-Regular.woff'
@@ -18,15 +21,25 @@ this.addEventListener('install', function (event) {
   );
 });
 
-this.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).catch(function () {
-    return fetch(event.request);
-  }).then(function (response) {
-    caches.open(version).then(function (cache) {
-      cache.put(event.request, response);
+function fromCache(request) {
+  return caches.open(version).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
     });
-    return response.clone();
-  }));
+  });
+}
+
+function update(request) {
+  return caches.open(version).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+this.addEventListener('fetch', function (event) {
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
 });
 
 this.addEventListener('activate', function (event) {
